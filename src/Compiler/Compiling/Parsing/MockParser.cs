0806053b6da55491f682e78c574bf55f3c4d14ed
Jan1902 <theredstoneritsme@gmail.com>
2021-12-30 15:17:51 +0100

    Added a Test Parser

---
+++ src/Compiler/Compiling/Parsing/MockParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerTest.Compiling.Parsing
+{
+    internal class MockParser : IParser
+    {
+        public Node Parse()
+        {
+            var ast = new Node(NodeType.Program);
+
+            ast.Children.Add(new Node(NodeType.Assignment)
+            {
+                Children = new List<Node>()
+                {
+                    new Node(NodeType.Variable, "a"),
+                    new Node(NodeType.Value, "1")
+                }
+            });
+
+            ast.Children.Add(new Node(NodeType.Assignment)
+            {
+                Children = new List<Node>()
+                {
+                    new Node(NodeType.Variable, "b"),
+                    new Node(NodeType.Value, "2")
+                }
+            });
+
+            ast.Children.Add(new Node(NodeType.Assignment)
+            {
+                Children = new List<Node>()
+                {
+                    new Node(NodeType.Variable, "c"),
+                    new Node(NodeType.Addition)
+                    {
+                        Children = new List<Node>()
+                        {
+                            new Node(NodeType.Variable, "a"),
+                            new Node(NodeType.Variable, "b")
+                        }
+                    }
+                }
+            });
+
+            return ast;
+        }
+    }
+}
